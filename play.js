24f117374de17164ca0879dbae6195e9777768a0
Nick Dellar <ndelfern@live.ca>
2022-09-09 04:17:56 +0000

    add input function to play.js

--- play.js
+++ play.js
@@ -2,3 +2,19 @@ const {connect, net} = require('./client');
 
 console.log("Connecting ...");
 connect();
+
+const handleUserInput = function (key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+setupInput();
